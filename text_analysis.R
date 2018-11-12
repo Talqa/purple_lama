6f163604769ff1f9b570d36c9730ad33b105d839
Alzbeta Talarovicova <7968267+Talqa@users.noreply.github.com>
2018-11-11 21:12:15 -0500

    word freq in tweet

--- text_analysis.R
+++ text_analysis.R
@@ -1,11 +1,42 @@
 library('tidyverse')
 library('tidytext')
 
+#get data
 data <- read_csv('Fortnite_tweets_test.csv')
 View(data)
-tweets_df <- as.data.frame(data$text) %>% 
-  rename(text = `data$text`)
-tweets_txt <- data$text
-View(tweets_txt)
-words <- tweets %>% 
+
+#select tweet text only
+tweets_df <- data %>%
+  select(text) %>% 
+  mutate(tweet_index = rownames(.))
+#View(tweets_df)
+
+#reshape text into words
+words <- tweets_df %>% 
   unnest_tokens(word, text)
+#View(words)
+
+#remove stop words and count
+tweet_words <- words %>% 
+  anti_join(stop_words) %>% 
+  count(word, sort = TRUE) %>% 
+  arrange(desc(n))
+View(tweet_words)
+
+#remove emoji
+cleaned_words <- tweet_words %>% 
+  filter(!grepl('1|2|3|4|5|6|7|8|9|0', word)) %>% 
+  arrange(desc(n))
+
+View(cleaned_words)
+
+
+#plot
+ggplot(subset(cleaned_words, n > 10), aes(reorder(word, n), n)) +
+  geom_point(size = 5, colour = 'purple') +
+#  geom_col() +
+  xlab(NULL) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  coord_flip()
+
